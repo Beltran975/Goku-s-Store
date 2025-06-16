11b640beb386647312ce88f230670d0348e0936d
Diego Beltran Hernandez <diegobeltran973@gmail.com>
2025-06-16 13:29:20 -0600

    Test

--- src/app/services/products.spec.ts
+++ src/app/services/products.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { ProductsService } from '../services/products';
 
-import { Products } from './products';
-
-describe('Products', () => {
-  let service: Products;
+describe('ProductsService', () => {
+  let service: ProductsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Products);
+    service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return products', () => {
+    const products = service.getProducts();
+    expect(products.length).toBeGreaterThan(0);
   });
 });
